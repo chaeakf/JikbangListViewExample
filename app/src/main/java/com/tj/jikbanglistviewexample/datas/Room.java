0e9216d540e15f115ecfc2567b3d6fe7f8a700f9
Eunsil-Jeong <daramchu@naver.com>
2019-04-20 15:44:30 +0900

    1) 데이터 객체 추출 2) 리스트 아이템XML <->  Adapter클래스 자바파일 연결 3) 데이터에 들어있는 값을 적절한 자리에 출력

--- app/src/main/java/com/tj/jikbanglistviewexample/datas/Room.java
+++ app/src/main/java/com/tj/jikbanglistviewexample/datas/Room.java
@@ -2,11 +2,11 @@ package com.tj.jikbanglistviewexample.datas;
 
 public class Room {
 
-    int deposit;    // 보증금
-    int month_pay;   // 월세
-    String location;    // eofkrwjrdls wldur
-    int floor;  // 몇층인지. 만약 지하라면 음수값. -2
-    String description; //설명 문구
+    public int deposit;    // 보증금
+    public int month_pay;   // 월세
+    public String location;    // eofkrwjrdls wldur
+    public int floor;  // 몇층인지. 만약 지하라면 음수값. -2
+    public String description; //설명 문구
 
     public Room(int deposit, int month_pay, String location, int floor, String description) {
         this.deposit = deposit;
