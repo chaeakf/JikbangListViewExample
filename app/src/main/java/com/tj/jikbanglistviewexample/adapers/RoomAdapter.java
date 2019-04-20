02691b19646f3eb52b6234291eabcfdc4faf0d8d
Eunsil-Jeong <daramchu@naver.com>
2019-04-20 15:04:59 +0900

    어댑터 클래스 생성, 생성자 작성, 사용할 변수 정의/ 값 대입

---
+++ app/src/main/java/com/tj/jikbanglistviewexample/adapers/RoomAdapter.java
@@ -0,0 +1,25 @@
+package com.tj.jikbanglistviewexample.adapers;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
+
+import com.tj.jikbanglistviewexample.R;
+import com.tj.jikbanglistviewexample.datas.Room;
+
+import java.util.List;
+
+public class RoomAdapter extends ArrayAdapter<Room> {
+
+    Context mContext;
+    List<Room> mList;
+    LayoutInflater inf;
+
+    public RoomAdapter(Context context, List<Room> list){
+        super(context, R.layout.room_list_item, list);
+
+        mContext = context;
+        mList = list;
+        inf = LayoutInflater.from(mContext);
+    }
+}
